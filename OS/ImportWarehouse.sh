f295f491a8668707dd4dd344e9989488fd37e021
Rick <rickjiangshu@gmail.com>
2017-09-05 13:59:39 +0800

    import tips

--- OS/ImportWarehouse.sh
+++ OS/ImportWarehouse.sh
@@ -3,11 +3,29 @@
 
 source ./CheckConfig.sh
 
+function ImportAll()
+{
+	echo "ImportAll"
+	return 0
+}
 
 if [ -f $WAREHOUSE_VERSION_FILE ]
 then
 	#进行差异导入
+	echo "差异导入"
 else
 	#全部导入
-
+	read -r -p "找不到$WAREHOUSE_VERSION_FILE，是否确定要全部导入？[y/n]" input
+	case $input in
+		[yY][eE][sS]|[yY])
+			ImportAll
+			;;
+		[nN][oO]|[nN])
+			echo "No"
+			;;
+		*)
+			echo "Invalid input..."
+			;;
+	esac
 fi
+
